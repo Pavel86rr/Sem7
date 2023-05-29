b6755941a300c0aeebcb7866575b3f34cc491dc9
«Pavel <kskles@yandex.ru>
2023-05-29 07:30:43 +0700

    Задача48

---
+++ Task48/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 48. Задайте двухмерный массив  размером m и n 
+//каждый элемент в массиве находится по формуле: Amn = m +n
+//Выведите полученный массив на экран
+// rows - 3; cols = 4.
+// 0 1 2 3
+// 1 2 2 4
+// 2 3 4 5
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] matrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+            //rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int[,] matrix = FillMatrix(ReadInt("Введите количество строк: "), ReadInt("Введите количество столбцов: "), 0, 99);
+PrintMatrix(matrix);
+
